519173b50af905e11ae27a2ffdc24d1a1fb37c7c
Abhay Stark <vermaabhay2323@gmail.com>
2024-08-28 02:26:23 +0530

    compass to atlas added books

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -20,7 +20,7 @@ function Cards({item}) {
                     <p>{item.title}</p>
                     <div className="card-actions justify-between">
                     <div className="badge badge-outline">₹{item.price}</div>
-                    <div className="cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">Buy Now</div>
+                    <a href={item.link} target="_blank" className="cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">Buy Now</a>
                     </div>
                 </div>
             </div>
